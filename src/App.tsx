dfb3450b51b7ee8c54e710aaa4c2b6f6a55054c0
Sasha Zaitsau <46328412+SashaZaicev@users.noreply.github.com>
2021-07-29 22:11:21 +0300

    create project, fix logo and name tab.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Welcome to weather module</h3>
       </header>
+      <div>
+
+      </div>
     </div>
   );
 }
