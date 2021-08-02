62b5eaded610472f66bb984ce0bc2c5e202b4dfa
Sasha Zaitsau <46328412+SashaZaicev@users.noreply.github.com>
2021-08-02 21:41:23 +0300

    fix css adapting

--- src/components/body/search/SearchSection.tsx
+++ src/components/body/search/SearchSection.tsx
@@ -50,7 +50,6 @@ export const SearchSection: FC<SearchSectionPropsType> = () => {
     }, [dispatch])
 
   useEffect(() => {
-    console.log(standCity)
     if (error || standCity) {
       setIsFetching(false)
       if (name !== sendName) {
